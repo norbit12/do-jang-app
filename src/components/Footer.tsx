9a5ade6a3c4058cb8590b9e0fcc6f1f2f36f4734
revelsprk <revelsprk@gmail.com>
2026-01-24 22:36:40 +0900

    Update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Footer() {
     return (
         <div>
-            <p className="text-center text-slate-400 text-sm mb-2">&copy; 2025 <Link href="/" className="underline focus:text-slate-600 outline-none hover:text-slate-600">DoJang</Link>. All rights reserved.</p>
+            <p className="text-center text-slate-400 text-sm mb-2">&copy; 2026 <Link href="/" className="underline focus:text-slate-600 outline-none hover:text-slate-600">DoJang</Link>. All rights reserved.</p>
         </div>
     )
 }
